4b4b172df863f89ec69339b3d6348262982081d5
INGLARD <44896145+INGLARD@users.noreply.github.com>
2020-06-26 11:51:48 +0200

    ajax_function_user

--- ajax/ajax_function_user.php
+++ ajax/ajax_function_user.php
@@ -5,7 +5,7 @@ include('../inc/functions_php/functions_user.php');
 $param1=$_GET['param1'];
 $param2=$_GET['param2'];
 $param3=$_GET['param3'];
-
-$resultats_user = getUser($dbconn2, $param1, $param2, $param3);
+$param4=$_GET['param4'];
+$resultats_user = getUser($dbconn2, $param1, $param2, $param3,$param4);
 
 ?>
